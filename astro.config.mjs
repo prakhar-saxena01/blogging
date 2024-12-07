a74b94c5098bdb6043774a8d1c791a1557c10a56
prakhar-saxena01 <prakhar_saxena@outlook.com>
2024-12-07 21:09:24 +0530

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  })
+});
